90796305fa7c44f9f6d73c527092e5cc54fe7313
sw5813 <sw5813@gmail.com>
2013-07-29 16:43:13 -0500

    Reset database and purged relationships- tank base 0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :followers, :following]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   before_filter :signed_in_user_filter, only: [:new, :create]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
-	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
-	has_many :followed_users, through: :relationships, source: :followed
-	has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
-	has_many :followers, through: :reverse_relationships, source: :follower
 
 	before_save { email.downcase! }
 	before_create :create_remember_token
@@ -25,18 +21,6 @@ class User < ActiveRecord::Base
 	def feed 
 		#prelim until ch. 11
 		Micropost.where("user_id = ?", id)
-	end
-
-	def following?(other_user)
-		relationships.find_by(followed_id: other_user.id)
-	end
-
-	def follow!(other_user)
-		relationships.create!(followed_id: other_user.id)
-	end
-
-	def unfollow!(other_user)
-		relationships.find_by(followed_id: other_user).destroy
 	end
 
 	private
