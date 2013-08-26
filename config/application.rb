e460f14dc709b2ef007c4ba021d96d8631881c77
sw5813 <sw5813@gmail.com>
2013-08-26 14:43:41 -0500

    precompile font assets

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module IdeaTank
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += %w(*.eot *.svg *.ttf *.woff)
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
